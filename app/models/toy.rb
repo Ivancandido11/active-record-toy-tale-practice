9540a7e48b8d3cc1f19a6f2fa0ad5e93686dca79
Ivan Candido <ivan.candido@powerhrg.com>
2021-08-31 16:35:11 -0400

    Completed Assignment

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -2,4 +2,15 @@ class Toy < ActiveRecord::Base
   has_many :purchases
   has_many :kids, through: :purchases
 
+  def self.most_expensive
+    find_by(price: Toy.maximum(:price))
+  end
+
+  def self.most_popular
+    Purchase.find_by(quantity: Purchase.maximum(:quantity)).toy
+  end
+
+  def kids_names
+    kids.pluck(:name)
+  end
 end
